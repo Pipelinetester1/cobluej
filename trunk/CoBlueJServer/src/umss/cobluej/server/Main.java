662320c568808f3eab6a54171190ecb8b27a7971
paolocastro.deb <paolocastro.deb@3516051b-7e1b-642c-26f8-303e440a8854>
2010-06-21 20:51:11 +0000

    CoBlueJServer project.

---
+++ trunk/CoBlueJServer/src/umss/cobluej/server/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package umss.cobluej.server;
+
+/**
+ *
+ * @author paolocastro
+ */
+public class Main {
+
+    /**
+     * Main class for the CoBlueJ Server.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
